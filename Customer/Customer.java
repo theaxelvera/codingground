98eefed3ac2af7dae0771847ff150f8c7669cfab
theaxelvera <avera38468@jcss.us>
2015-12-16 13:16:41 -0500

    Created by Coding Ground Client

---
+++ Customer/Customer.java
@@ -0,0 +1,15 @@
+public class Customer
+{
+public String name;
+double totalSales;
+}   
+public Customer(String customerName, double amountBought);
+{
+name = customerName;
+totalSales = amountBought;
+}
+public String newName()
+{
+return name;
+}
+}
