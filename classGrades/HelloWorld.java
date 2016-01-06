59d7cbfd9e4656a0c17bfe47986d20518ea4fd00
theaxelvera <avera38468@jcss.us>
2016-01-06 13:13:58 -0500

    Created by Coding Ground Client

---
+++ classGrades/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        int[] classGrades = new int[10];
+        classGrades[0] = 95;
+        classGrades[1] = 42;
+        classGrades[2] = 57;
+        classGrades[3] = 69;
+        classGrades[4] = 100;
+        classGrades[5] = 55;
+        classGrades[6] = 87;
+        classGrades[7] = 93;
+        classGrades[8] = 87;
+        classGrades[9] = 88;
+        System.out.println(classGrades[2]);
+        System.out.println(classGrades[3]);
+        System.out.println(classGrades[4]);
+        System.out.println((classGrades[4])-(classGrades[5])+(classGrades[0]));
+        System.out.println("The first number is " + classGrades[0]);
+     }
+}
